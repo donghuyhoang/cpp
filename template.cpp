a94c78613cca85cae52e8f9118e6f265bc7736bb
Dong Huy Hoang <70799199+donghuyhoang@users.noreply.github.com>
2025-08-26 10:50:27 +0700

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -1,27 +1,30 @@
-   /* donghuyhoang */
+    // have a good day !
+    /* dong` huy hoang` */
+ 
 #include<bits/stdc++.h>
 using namespace std;
-#define fi first                        
+#define fi first
 #define se second
 #define in insert
 #define pb push_back
 #define endl '\n'
-#define L(s) (int)s.length()
-#define FOR(i,x,n) for(int i = x ; i <= (int)n ; i++)
-#define FORR(i,x,n) for(int i = x ; i >= (int)n ; i--)
 #define ms(a,x) memset(a,x,sizeof a)
-typedef long long ll;
-typedef unsigned long long ull;
-using P = pair<int,int>;
-const int MOD = (int) 1e9+7;
+#define fillvalue(a,x) fill(a,a + sizeof(a) / sizeof(a[0]),x)
+#define FOR(i,x,n) for(int i = x; i <= n; i++)
+#define FORR(i,x,n) for(int i = x; i >= n; i--)
 inline ll gcd(ll a,ll b){if(b<=0) return a; return gcd(b,a%b);}
 inline ll lcm(ll a,ll b){return a/gcd(a,b)*b;}
+typedef long long ll;
+typedef unsigned long long ull;
+const int MOD =  1e9+7;
+const int inf = 2e9;
+const int uinf = INT_MIN;
+const long long INF = 9e18;
+const long long UINF = LLONG_MIN;
+const int N = 5e5 + 5;
 
-int main(){                           
-//    freopen("input.txt","r",stdin);                                               
-//    freopen("output.txt","w",stdout);
-  ios_base::sync_with_stdio(false); 
-  cin.tie(NULL);
-  cerr << "\nTime: " << 1000 * clock() / CLOCKS_PER_SEC << "ms\n";
-  return 0;
-}                            
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+}
