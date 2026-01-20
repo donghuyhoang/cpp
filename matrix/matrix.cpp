bd423107aa5277c323c832ff07110e8c6fe9d2df
Dong Huy Hoang <70799199+donghuyhoang@users.noreply.github.com>
2026-01-20 22:30:23 +0700

    Refactor Matrix class and add Cholesky methods
    Updated Matrix class to use double instead of int for better precision. Added Cholesky decomposition methods and symmetry check.

--- matrix/matrix.cpp
+++ matrix/matrix.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 #include <vector>
+#include <math.h>
 using namespace std;
-
 struct Matrix{
     int n,m;
-    vector<vector<int>> x;
-    Matrix(int n, int m) : n(n),m(m){
-        x.assign(n,vector<int>(m,0));
+    vector<vector<double>> x;
+    Matrix(int n,int m) : n(n), m(m){
+        x.assign(n, vector<double>(m,0));
     }
     void input(){
         for (int i = 0; i < n; i++){
@@ -23,13 +23,20 @@ struct Matrix{
             cout << endl;
         }
     }
+    void outputTranfer(){
+        for (int i = 0; i < n; i++){
+            for (int j = 0; j < m; j++){
+                cout << x[j][i] << ' ';
+            }
+            cout << endl;
+        }
+    }
     friend Matrix operator*(const Matrix&, const Matrix&);
 };
 Matrix operator*(const Matrix& a, const Matrix& b){
     Matrix c(a.n,b.m);
-    int n = a.n,m = b.m;
-    for (int i = 0; i < n; i++){
-        for (int j = 0; j < m; j++){
+    for (int i = 0; i < a.n; i++){
+        for (int j = 0; j < b.m; j++){
             long long res = 0;
             for (int l = 0; l < a.m; l++){
                 res += a.x[i][l] * b.x[l][j];
@@ -39,12 +46,90 @@ Matrix operator*(const Matrix& a, const Matrix& b){
     }
     return c;
 }
-
-int main() {
-    int n,m; cin >> n >> m;
-    Matrix M(n,m),M1(n,m);
-    M.input();
-    M1.input();
-    Matrix M2 = M * M1;
-    M2.output();
+Matrix Tranfer(Matrix a){
+    Matrix b(a.m,a.n);
+    for (int i = 0; i < a.n; i++){
+        for (int j = 0; j < a.m; j++){
+            b.x[j][i] = a.x[i][j];
+        }
+    }
+    return b;
 }
+Matrix choleskyDecomposition(Matrix a){
+    int n = a.n;
+    Matrix lower(n,n);
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j <= i; j++){
+            double sum = 0;
+            if(i == j){
+                for (int k = 0; k < j; k++){
+                    sum += pow(lower.x[j][k],2);
+                }
+                lower.x[j][j] = sqrt(a.x[j][j] - sum);
+            }
+            else{
+                for (int k = 0; k < j; k++){
+                    sum += lower.x[i][k] * lower.x[j][k];
+                }
+                lower.x[i][j] = (a.x[i][j] - sum) / lower.x[j][j];
+            }
+        }
+    }
+    return lower;
+}
+
+pair<Matrix,Matrix> choleskyDecompositionModified(Matrix a){
+    int n = a.n;
+    Matrix L(n,n),D(n,n);
+    for (int i = 0; i < n; i++) L.x[i][i] = 1;
+    for (int j = 0; j < n; j++){
+        double sum = 0;
+        for (int k = 0; k < j; k++){
+            sum += L.x[j][k] * L.x[j][k] * D.x[k][k];
+        }
+        D.x[j][j] = a.x[j][j] - sum;
+        for (int i = j + 1; i < n; i++){
+            sum = 0;
+            for (int k = 0; k < j; k++){
+                sum += L.x[i][k] * L.x[j][k] * D.x[k][k];
+            }
+            L.x[i][j] = (a.x[i][j] - sum) / D.x[j][j];
+        }
+    }
+    return {L,D};
+}
+
+bool isSymmetry(Matrix a){
+    bool res = true;
+    int n = a.n;
+    for (int i = 0; i < n; i++){
+        for (int j = i + 1; j < n; j++){
+            if(a.x[i][j] != a.x[j][i]) res = false;
+        }
+    }
+    return res;
+}
+
+int main(){
+    cout << "Nhap so cot so hang ma tran vuong: ";
+    int n; cin >> n;
+    Matrix A(n,n);
+    A.input();
+    if(!isSymmetry(A)){
+        cout << "Ma tran khong doi xung, xin nhap lai";
+        return 0;
+    }
+    cout << "Su dung cho Cholesky co dien: " << endl;
+    Matrix L = choleskyDecomposition(A);
+    cout << "L:" << endl; L.output();
+    cout << endl;
+    cout << "L^T" << endl; L.outputTranfer();
+    cout << "Su dung Cholesky bien the: " << endl;
+    pair<Matrix,Matrix> p = choleskyDecompositionModified(A);
+    cout << "K: " << endl; p.first.output();
+    cout << "D: " << endl; p.second.output();
+    cout << "K^T: " << endl; p.first.outputTranfer();
+    Matrix L_T = Tranfer(L);
+    Matrix check = L * L_T;
+    cout << "Check cholesky L* L^T: "; check.output();
+ }
